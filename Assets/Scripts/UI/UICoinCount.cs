670a3517e6da5f62923ee10b2b9b2bf2c78e2969
Andrew Zimmerman <astevezimm@gmail.com>
2021-02-17 15:28:59 -0700

    Attempt 2

---
+++ Assets/Scripts/UI/UICoinCount.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UICoinCount : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        Coin.OnCoinCollected += coins => text.text = coins.ToString();
+    }
+}
