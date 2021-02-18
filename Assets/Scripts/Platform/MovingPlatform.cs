b9d10db6601d66798ddc674fa13e63c6c4cec9f9
Andrew Zimmerman <astevezimm@gmail.com>
2021-02-18 16:14:37 -0700

    Continue implementing moving platforms

--- Assets/Scripts/Platform/MovingPlatform.cs
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -1,29 +1,37 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform[] wayPoints;
     [SerializeField] private float speed = 1;
-    [SerializeField] private bool pingPong = true;
+    [SerializeField] private PlatformMovementType type;
 
-    private Vector3 _nextPoint;
-    private bool _forward = true;
+    private IPlatformIter _platformIter;
 
     private void Start()
     {
-        if (wayPoints == null)
-            wayPoints = new[] {transform};
-        _nextPoint = wayPoints[0].position;
+        wayPoints ??= new[] {transform};
+        _platformIter = type switch
+        {
+            PlatformMovementType.Loop => new LoopPlatformIter(wayPoints),
+            PlatformMovementType.PingPong => new PingPongPlatformIter(wayPoints),
+            _ => new SingPassPlatformIter(wayPoints)
+        };
+        StartCoroutine(Tick());
     }
 
-    private void Update()
+    private IEnumerator Tick()
     {
-        float delta = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, _nextPoint, delta);
-        if (transform.position == _nextPoint)
+        while (true)
         {
-            
+            float delta = speed * Time.deltaTime;
+            transform.position =
+                Vector3.MoveTowards(transform.position, _platformIter.NextPoint, delta);
+            if (transform.position == _platformIter.NextPoint)
+                yield return _platformIter.ChangeNextPoint();
+            yield return null;
         }
     }
 }
